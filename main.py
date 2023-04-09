4700e5b1f97543a9b506a52d7de5035dba02224f
Roberto Albalate <albalaterober@gmail.com>
2023-04-09 16:50:45 +0200

    Creating lists

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
+ 
+from math import pi, floor
 
 
+
+lista=[23,"dos","perro",23,45]
+
+
+def isnumber(item): 
+    result = type(item) in (int,float)  
+    return result 
 
 def main ():
-    print("hello world!")
+    
+    lista2 = [isnumber(val) for val in lista]
+    print(lista2)
 
+
+    
 main()
+
+
+
+
+
